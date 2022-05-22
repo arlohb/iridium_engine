ae5f1d389d28d03b70908a82cd04b5b0b18d9ba9
arlohb <arlo@urwick.co.uk>
2022-05-22 11:31:48 +0100

    Added simple render window

---
+++ iridium_editor/src/app.rs
@@ -0,0 +1,99 @@
+use winit::{
+    window::Window,
+    event::WindowEvent,
+};
+
+pub struct App {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    surface_config: wgpu::SurfaceConfiguration,
+    pub surface_size: winit::dpi::PhysicalSize<u32>,
+}
+
+impl App {
+    pub async fn new(window: &Window) -> Self {
+        let surface_size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::Backends::all());
+        let surface = unsafe { instance.create_surface(window) };
+        let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        }).await.unwrap();
+
+        let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor {
+            features: wgpu::Features::empty(),
+            limits: wgpu::Limits::default(),
+            label: None,
+        }, None).await.unwrap();
+
+        let surface_config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface.get_preferred_format(&adapter).unwrap(),
+            width: surface_size.width,
+            height: surface_size.height,
+            present_mode: wgpu::PresentMode::Mailbox,
+        };
+        surface.configure(&device, &surface_config);
+
+        Self {
+            surface,
+            device,
+            queue,
+            surface_config,
+            surface_size,
+        }
+    }
+
+    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.surface_size = new_size;
+            self.surface_config.width = new_size.width;
+            self.surface_config.height = new_size.height;
+            self.surface.configure(&self.device, &self.surface_config);
+        }
+    }
+
+    pub fn input(&mut self, event: &WindowEvent) -> bool {
+        false
+    }
+
+    pub fn update(&mut self) {}
+
+    pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                }],
+                depth_stencil_attachment: None,
+            });
+        }
+        
+        self.queue.submit(std::iter::once(encoder.finish()));
+        // self.queue.submit([encoder.finish()]);
+        output.present();
+
+        Ok(())
+    }
+}
--- iridium_editor/src/main.rs
+++ iridium_editor/src/main.rs
@@ -2,10 +2,28 @@ mod components;
 use components::*;
 mod systems;
 use systems::*;
+mod app;
+use app::*;
 
 use iridium_ecs::*;
 
-fn main() {
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .build(&event_loop)
+        .unwrap();
+    
+    let mut app = App::new(&window).await;
+
     let mut world = World::new(
         Entities::new(vec![
             Entity::new(vec![
@@ -19,7 +37,42 @@ fn main() {
         ]
     );
 
-    loop {
-        world.run_systems();
-    }
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => if !app.input(event) { match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                input: KeyboardInput {
+                    state: ElementState::Pressed,
+                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                    ..
+                },
+                ..
+            } => {
+                *control_flow = ControlFlow::Exit;
+            },
+            WindowEvent::Resized(physical_size) => {
+                app.resize(*physical_size);
+            },
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                app.resize(**new_inner_size);
+            },
+            _ => {}
+        }},
+        Event::RedrawRequested(window_id) if window_id == window.id() => {
+            app.update();
+            match app.render() {
+                Ok(_) => {},
+                Err(wgpu::SurfaceError::Lost) => app.resize(app.surface_size),
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                Err(err) => panic!("{:?}", err),
+            }
+        },
+        Event::MainEventsCleared => {
+            window.request_redraw();
+        },
+        _ => {}
+    });
 }
