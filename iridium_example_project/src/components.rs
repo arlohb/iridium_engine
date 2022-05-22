bef89384164db8fa63873c07ad49d39c89d62d08
arlohb <arlo@urwick.co.uk>
2022-05-22 12:18:36 +0100

    Added example project

---
+++ iridium_example_project/src/components.rs
@@ -0,0 +1,6 @@
+use iridium_ecs_macros::*;
+
+#[derive(Component)]
+pub struct CustomComponent {
+    pub value: u32,
+}
---
+++ iridium_example_project/src/systems.rs
@@ -0,0 +1,17 @@
+use iridium_ecs::*;
+use iridium_ecs_macros::*;
+use crate::components::*;
+
+#[derive(System)]
+pub struct CustomSystem {
+    pub activated: bool,
+}
+
+impl CustomSystem {
+    fn run(&mut self, entities: &mut Entities, _delta_time: f64) {
+        for entity in entities.query_1::<CustomComponent>().iter() {
+            let custom_component = entity.get_component::<CustomComponent>().unwrap();
+            println!("Custom component value: {}", custom_component.value);
+        }
+    }
+}
