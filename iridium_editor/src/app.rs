c0469a44f08b6f5a53c7bdb0137244fe73f5a50f
arlohb <arlo@urwick.co.uk>
2022-06-08 13:23:05 +0100

    Made the UI smaller

--- iridium_editor/src/app.rs
+++ iridium_editor/src/app.rs
@@ -56,21 +56,21 @@ impl App {
                 ScreenRect::new(
                     0.,
                     0.,
-                    1.,
-                    0.8,
+                    0.4,
+                    0.2,
                 )
             ),
-            EguiPanel::new(
-                &device,
-                surface_config.format,
-                egui_demo_lib::DemoWindows::default(),
-                ScreenRect::new(
-                    0.,
-                    0.,
-                    1.,
-                    0.8,
-                )
-            ),
+            // EguiPanel::new(
+            //     &device,
+            //     surface_config.format,
+            //     egui_demo_lib::DemoWindows::default(),
+            //     ScreenRect::new(
+            //         0.,
+            //         0.,
+            //         1.,
+            //         0.8,
+            //     )
+            // ),
         ];
 
         Self {
