504ac08e9be41d30389ca3e979ab6b4c083eca9e
arlohb <arlo@urwick.co.uk>
2022-06-13 08:45:53 +0100

    Right panel now shows which components are on the selected entity

--- iridium_ecs/src/entities.rs
+++ iridium_ecs/src/entities.rs
@@ -3,7 +3,8 @@ use std::sync::{Mutex, MutexGuard};
 use hashbrown::HashMap;
 
 pub struct Entities {
-    entities: Vec<u128>,
+    /// entity_id => components
+    entities: HashMap<u128, Vec<String>>,
     /// component_type => entity_id => component
     components: HashMap<String, HashMap<u128, Mutex<Component>>>,
     pub component_types: HashMap<String, ComponentType>,
@@ -12,32 +13,35 @@ pub struct Entities {
 impl Entities {
     pub fn new(component_types: HashMap<String, ComponentType>) -> Entities {
         Entities {
-            entities: vec![],
+            entities: HashMap::new(),
             components: HashMap::new(),
             component_types,
         }
     }
 
     pub fn add_components(&mut self, entity_id: u128, components: Vec<Component>) {
-        components.into_iter().for_each(|component| {
-            match self.components.contains_key(&component.name) {
-                true => {
-                    self.components.get_mut(&component.name).unwrap().insert(entity_id, Mutex::new(component));
-                }
-                false => {
-                    let name = component.name.clone();
-                    let mut components = HashMap::new();
-                    components.insert(entity_id, Mutex::new(component));
-                    self.components.insert(name, components);
-                }
+        if !self.entities.contains_key(&entity_id) {
+            self.entities.insert(entity_id, vec![]);
+        }
+
+        for component in components {
+            self.entities.get_mut(&entity_id).unwrap().push(component.name.clone());
+
+            if self.components.contains_key(&component.name) {
+                self.components.get_mut(&component.name).unwrap().insert(entity_id, Mutex::new(component));
+            } else {
+                let name = component.name.clone();
+                let mut components = HashMap::new();
+                components.insert(entity_id, Mutex::new(component));
+                self.components.insert(name, components);
             }
-        })
+        }
     }
 
     pub fn new_entity(&mut self, name: &str, components: Vec<Component>) -> u128 {
         let id = uuid::Uuid::new_v4().as_u128();
 
-        self.entities.push(id);
+        self.entities.insert(id, vec![]);
 
         self.add_components(id, vec![
             create_component! { Name
@@ -48,6 +52,21 @@ impl Entities {
         self.add_components(id, components);
 
         id
+    }
+
+    pub fn get_entity_component_types(&self, entity_id: u128) -> Vec<String> {
+        self.entities.get(&entity_id).unwrap().clone()
+    }
+
+    pub fn get_entity_components(&self, entity_id: u128) -> Vec<MutexGuard<Component>> {
+        let component_types = self.get_entity_component_types(entity_id);
+
+        component_types.into_iter().map(|component_type| {
+            let entities_to_components = self.components.get(&component_type).unwrap();
+            let component_mutex = entities_to_components.get(&entity_id).unwrap();
+
+            component_mutex.lock().unwrap()
+        }).collect::<Vec<_>>()
     }
 
     pub fn query_with_id<const N: usize>(
--- iridium_editor/src/ui/panels/components_list.rs
+++ iridium_editor/src/ui/panels/components_list.rs
@@ -24,21 +24,11 @@ impl PanelUi for ComponentsList {
             ui.separator();
             ui.add_space(10.);
 
-            for (id, [name])
-            in world.entities.query_with_id(["Name"]) {
-                let name = name.get::<String>("name");
+            if let Some(id) = ui_state.selected_entity {
+                let components = world.entities.get_entity_components(id);
 
-                let mut rich_text = egui::RichText::new(name);
-
-                if let Some(selected_id) = ui_state.selected_entity {
-                    if selected_id == id {
-                        rich_text = rich_text.strong();
-                    }
-                }
-
-                if ui.add(egui::Label::new(rich_text).sense(egui::Sense::click())).clicked() {
-                    println!("Clicked id: {id}");
-                    ui_state.selected_entity = Some(id);
+                for component in components {
+                    ui.label(component.name.to_string());
                 }
             }
         });
