b07e72da0729f92ca29c50dbf72386ed5c2d83c8
arlohb <arlo@urwick.co.uk>
2022-06-16 20:46:31 +0100

    Removed old system derive macro

--- iridium_ecs_macros/src/lib.rs
+++ iridium_ecs_macros/src/lib.rs
@@ -2,6 +2,8 @@ use proc_macro::TokenStream;
 use syn::parse_macro_input;
 use quote::quote;
 
+// I am sure this will be used at some point.
+#[allow(dead_code)]
 fn get_struct_fields(ast: &syn::DeriveInput) -> Vec<&syn::Field> {
     match &ast.data {
         syn::Data::Struct(data) => data.fields.iter().collect(),
@@ -21,45 +23,4 @@ pub fn derive_component_trait(tokens: TokenStream) -> TokenStream {
             }
         }
     }.to_string().parse().unwrap()
-}
-
-#[proc_macro_derive(System)]
-pub fn derive_system(tokens: TokenStream) -> TokenStream {
-    let ast = parse_macro_input!(tokens as syn::DeriveInput);
-    let struct_name = ast.ident.clone();
-
-    let fields = get_struct_fields(&ast);
-
-    ((match fields.len() {
-        1 => quote! {
-            impl #struct_name {
-                pub fn new(activated: bool) -> Self {
-                    Self {
-                        activated,
-                    }
-                }
-            }
-        }.to_string(),
-        _ => "".to_string()
-    }) + &quote! {
-        impl iridium_ecs::systems::System for #struct_name {
-            fn name(&self) -> &'static str {
-                stringify!(#struct_name)
-            }
-
-            fn get_activated(&self) -> bool {
-                self.activated
-            }
-
-            fn set_activated(&mut self, activated: bool) {
-                self.activated = activated;
-            }
-
-            fn run_system(&mut self, entities: &iridium_ecs::Entities, delta_time: f64) {
-                self.run(entities, delta_time);
-            }
-        }
-    }.to_string())
-        .parse()
-        .unwrap()
 }
