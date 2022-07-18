652bc0e76af07121323abae7ed9581c9c1a0ecbb
arlohb <arlo@urwick.co.uk>
2022-07-18 21:34:43 +0100

    Added notes.rs for how things could work.

---
+++ notes.rs
@@ -0,0 +1,25 @@
+mod stages {
+    pub trait ShaderStage {}
+
+    pub struct VertexShaderStage;
+    impl ShaderStage for VertexShaderStage {}
+
+    pub struct FragmentShaderStage;
+    impl ShaderStage for FragmentShaderStage {}
+}
+
+pub trait ShaderTrait {
+    type Stage: stages::ShaderStage;
+}
+
+pub struct DefaultVertexShader;
+
+impl ShaderTrait for DefaultVertexShader {
+    type Stage = stages::VertexShaderStage;
+}
+
+pub struct DefaultFragmentShader;
+
+impl ShaderTrait for DefaultFragmentShader {
+    type Stage = stages::FragmentShaderStage;
+}
