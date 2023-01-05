4ea2f73b60be55d54656db18ac4a83c26e6fea59
arlohb <arlo@urwick.co.uk>
2023-01-05 10:04:12 +0000

    Improved asset loading for example project

--- iridium_example_project/src/assets.rs
+++ iridium_example_project/src/assets.rs
@@ -3,6 +3,76 @@ use iridium_assets::Assets;
 use iridium_graphics::{Material, Mesh, Shader, ShaderInput, ShaderType, Texture, Vertex};
 use iridium_maths::VecN;
 
+/// Load a set of assets for a sprite.
+///
+/// This is specific to this game,
+/// so textures are not filtered,
+/// it requires `sprite_vertex` to be loaded,
+/// and it assumes every sprite has the same material.
+///
+/// Will create:
+/// - `XXX_tex` - texture
+/// - `XXX_frag` - fragment shader
+/// - `XXX_mat` - material
+fn load_sprite_assets(
+    sprite_data: Vec<(&str, &str)>,
+    frag_shader_spirv: &[u32],
+    camera_gpu_data: &iridium_graphics::CameraGpuData,
+    device: &wgpu::Device,
+    queue: &wgpu::Queue,
+    surface_format: wgpu::TextureFormat,
+    assets: &mut Assets,
+) {
+    for (name, image_path) in sprite_data {
+        assets.add(
+            &format!("{name}_tex"),
+            Texture::from_image_bytes(
+                device,
+                queue,
+                &std::fs::read(format!("iridium_example_project/assets/{image_path}"))
+                    .expect("Failed to read image"),
+                false,
+            ),
+        );
+
+        assets.add(
+            &format!("{name}_frag"),
+            Shader::new(
+                device,
+                ShaderType::Fragment,
+                frag_shader_spirv,
+                vec![
+                    ShaderInput::Texture(
+                        assets
+                            .get::<Texture>(&format!("{name}_tex"))
+                            .unwrap_or_else(|| panic!("Could not find {name}_tex")),
+                    ),
+                    ShaderInput::Sampler(
+                        assets
+                            .get::<Texture>(&format!("{name}_tex"))
+                            .unwrap_or_else(|| panic!("Could not find {name}_tex")),
+                    ),
+                ],
+            ),
+        );
+
+        assets.add(
+            &format!("{name}_mat"),
+            Material::new(
+                device,
+                surface_format,
+                assets
+                    .get::<Shader>("sprite_vertex")
+                    .expect("asset 'sprite_vertex' not found"),
+                camera_gpu_data,
+                assets
+                    .get::<Shader>(&format!("{name}_frag"))
+                    .unwrap_or_else(|| panic!("Could not find {name}_frag")),
+            ),
+        );
+    }
+}
+
 /// Load the assets needed for the game.
 #[no_mangle]
 pub fn load_assets(
@@ -13,93 +83,12 @@ pub fn load_assets(
     assets: &mut Assets,
 ) {
     assets.add(
-        "fish_tex",
-        Texture::from_image_bytes(
-            device,
-            queue,
-            include_bytes!("../assets/FoodSprites/Food/Fish.png"),
-            false,
-        ),
-    );
-
-    assets.add(
         "sprite_vertex",
         Shader::new(
             device,
             ShaderType::Vertex,
             include_spirv!("assets/vert.hlsl", vert, hlsl, entry = "vs_main", no_debug),
             vec![ShaderInput::Transform],
-        ),
-    );
-    assets.add(
-        "fish_frag",
-        Shader::new(
-            device,
-            ShaderType::Fragment,
-            include_spirv!(
-                "assets/sprite.hlsl",
-                frag,
-                hlsl,
-                entry = "fs_main",
-                no_debug
-            ),
-            vec![
-                ShaderInput::Texture(
-                    assets
-                        .get::<Texture>("fish_tex")
-                        .expect("asset 'steak_tex' not found"),
-                ),
-                ShaderInput::Sampler(
-                    assets
-                        .get::<Texture>("fish_tex")
-                        .expect("asset 'steak_tex' not found"),
-                ),
-            ],
-        ),
-    );
-    assets.add(
-        "uv_test_fragment",
-        Shader::new(
-            device,
-            ShaderType::Fragment,
-            include_spirv!(
-                "assets/uv_test.hlsl",
-                frag,
-                hlsl,
-                entry = "fs_main",
-                no_debug
-            ),
-            vec![],
-        ),
-    );
-
-    assets.add(
-        "fish_mat",
-        Material::new(
-            device,
-            surface_format,
-            assets
-                .get::<Shader>("sprite_vertex")
-                .expect("asset 'sprite_vertex' not found"),
-            camera_gpu_data,
-            assets
-                .get::<Shader>("fish_frag")
-                .expect("asset 'sprite_fragment' not found"),
-        ),
-    );
-
-    assets.add(
-        "uv_test",
-        Material::new(
-            device,
-            surface_format,
-            assets
-                .get::<Shader>("sprite_vertex")
-                .expect("asset 'sprite_vertex' not found"),
-            camera_gpu_data,
-            assets
-                .get::<Shader>("uv_test_fragment")
-                .expect("asset 'uv_test_fragment' not found"),
         ),
     );
 
@@ -114,5 +103,24 @@ pub fn load_assets(
             ],
             indices: vec![0, 3, 2, 0, 2, 1],
         },
+    );
+
+    load_sprite_assets(
+        vec![
+            ("fish", "FoodSprites/Food/Fish.png"),
+            ("steak", "FoodSprites/Food/Steak.png"),
+        ],
+        include_spirv!(
+            "assets/sprite.hlsl",
+            frag,
+            hlsl,
+            entry = "fs_main",
+            no_debug
+        ),
+        camera_gpu_data,
+        device,
+        queue,
+        surface_format,
+        assets,
     );
 }
