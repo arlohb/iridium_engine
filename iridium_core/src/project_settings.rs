60a40121cd607d1cbc8fc1a4050b59d79c260829
arlohb <arlo@urwick.co.uk>
2023-02-16 06:58:45 +0000

    Better doc

--- iridium_core/src/project_settings.rs
+++ iridium_core/src/project_settings.rs
@@ -1,4 +1,4 @@
-/// Settings about the project.
+/// Stores engine / runtime settings specific to the project.
 pub struct ProjectSettings {
     /// The default scene to open when the editor opens or when the game starts.
     pub default_scene: String,
