4a9f1f691d8fd69e282e5303ed7e5c20364f188e
arlohb <arlo@urwick.co.uk>
2023-03-03 11:45:48 +0000

    Changed uv_test to fully opaque

--- iridium_example_project/assets/uv_test.hlsl
+++ iridium_example_project/assets/uv_test.hlsl
@@ -4,5 +4,5 @@ struct VertexOut {
 };
 
 float4 fs_main(VertexOut vertex) : SV_TARGET {
-    return float4(float3(vertex.uv_coords, 0.), 0.5);
+    return float4(float3(vertex.uv_coords, 0.), 1.0);
 }
